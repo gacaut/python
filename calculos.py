c4650dad8b872c9b8cb65df714853ddf280a08da
gacaut <129332059+gacaut@users.noreply.github.com>
2023-07-05 23:24:57 -0300

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -49,8 +49,7 @@ def burbuja(lista):
     desordenado = True
     while desordenado:
         desordenado = False
-        for i in range(len(lista)-1):
-            
+        for i in range(len(lista)-1):            
             if lista[i] < lista[i+1]:
                 aux = lista[i]
                 lista[i] = lista[i+1]
